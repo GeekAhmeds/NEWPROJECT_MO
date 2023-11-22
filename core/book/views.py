dd5afed4a67a8da85f2e3d13e8dba81152ae5d29
AhmedS <geekahmeds@gmail.com>
2023-11-22 20:52:59 +0200

    Add API For Model Book

--- core/book/views.py
+++ core/book/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Books
+from .serializers import CategorySerializer, BooksSerializer
 
-# Create your views here.
+
+
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class BooksViewSet(viewsets.ModelViewSet):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
