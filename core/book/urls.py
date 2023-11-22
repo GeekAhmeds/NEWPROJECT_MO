c9e0d5d683b98c6571c6231c4e8ac8446ca669b4
AhmedS <geekahmeds@gmail.com>
2023-11-22 19:10:58 +0200

    First Push

---
+++ core/book/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+# to show images
+# from django.contrib.staticfiles.urls import static
+
+urlpatterns = [
+
+    # path('', include('book.urls')),
+
+
+    # Djoser
+
+]
